56deb0ef7920c08429ff5a73a1d8a23f5f4d4b64
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2024-01-18 20:46:42 -0500

    adds form reset after submit

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./UploadForm.css";
@@ -10,14 +12,25 @@ import "react-toastify/dist/ReactToastify.css";
 function UploadForm() {
   const navigate = useNavigate();
 
-  const [ image, setImage] = useState();
-  const [form, setForm] = useState({ created_by: "", tag: "", existing_url: '' , file: '' });
-  const [ memeInfo, setMemeInfo ] = useState({ memeUrl: '' , file_name: '',  etag: '',  width: "", height: '', });
+  const [image, setImage] = useState();
+  const [form, setForm] = useState({
+    created_by: "",
+    tag: "",
+    existing_url: "",
+    file: "",
+  });
+  const [memeInfo, setMemeInfo] = useState({
+    memeUrl: "",
+    file_name: "",
+    etag: "",
+    width: "",
+    height: "",
+  });
 
-
-const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
-const baseUrl = import.meta.env.VITE_BASE_URL || 'http://localhost:8080'
-const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api/v1/meme/add";
+  const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
+  const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:8080";
+  const UPLOAD_URL =
+    import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api/v1/meme/add";
 
   const handleForm = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -26,9 +39,8 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
   const handleImage = async (e) => {
     // const image = e.target.files[0]
     // setForm({ ...form, [e.target.name]: e.target.value });
-   // const image =  e.target.files[0] || e.target.value;
-   const image = form.existing_url ? form.existing_url : e.target.files[0];
-
+    // const image =  e.target.files[0] || e.target.value;
+    const image = form.existing_url ? form.existing_url : e.target.files[0];
     setImage(image);
 
     //console.log(image);
@@ -53,47 +65,45 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
   };
 
   const handleUpload = async (e) => {
-
     e.preventDefault();
 
-       if (!memeInfo.memeUrl) {
-         toast.error("Please upload a meme before submitting");
-         return;
-       }
-
-    try {
-          await axios.post(UPLOAD_URL, {
-            created_by: form.created_by,
-            meme_url: memeInfo.memeUrl,
-            file_name: memeInfo.file_name,
-            tag: form.tag,
-            height: memeInfo.height,
-            width: memeInfo.width,
-            etag: memeInfo.etag,
-            type: memeInfo.type
-          });
-
-          toast.success('Meme uploaded! 👍',  {
-            position: "top-right",
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-    } catch (error) {
-
-    } finally {
-      setForm({ created_by: "", tag: "", existing_url: "", file: "" });
-
+    if (!memeInfo.memeUrl) {
+      toast.error("Please upload a meme before submitting");
+      return;
     }
 
+    try {
+      await axios.post(UPLOAD_URL, {
+        created_by: form.created_by,
+        meme_url: memeInfo.memeUrl,
+        file_name: memeInfo.file_name,
+        tag: form.tag,
+        height: memeInfo.height,
+        width: memeInfo.width,
+        etag: memeInfo.etag,
+        type: memeInfo.type,
+      });
+
+      toast.success("Meme uploaded! 👍", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } catch (error) {
+    } finally {
+      setForm({
+        created_by: "",
+        tag: "",
+        existing_url: "",
+        file: "",
+      });
+    }
   };
-
-
-
 
   return (
     <>
@@ -102,7 +112,7 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
         <section className="">
           <div className="main-container">
             <div className="">
-              <form className="form">
+              <form className="form" id="form">
                 {form.existing_url ? (
                   <div className="label-input-div">
                     <label htmlFor="file">Upload a meme</label>
@@ -110,6 +120,7 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
                       id="file-field"
                       type="file"
                       name="file"
+                      value={form.file}
                       onChange={(e) => handleImage(e)}
                       disabled
                     />
@@ -121,6 +132,7 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
                       id="file-field"
                       type="file"
                       name="file"
+                      value={form.file}
                       onChange={(e) => {
                         handleForm(e);
                         handleImage(e);
@@ -135,6 +147,7 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
                     <input
                       id="file-field"
                       type="text"
+                      value={form.existing_url}
                       name="existing_url"
                       onChange={(e) => {
                         handleImage(e);
@@ -147,6 +160,7 @@ const UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL || "http://localhost:8080/api
                     <label htmlFor="file">Paste a meme url</label>
                     <input
                       id="file-field"
+                      value={form.existing_url}
                       type="text"
                       name="existing_url"
                       onChange={(e) => {
